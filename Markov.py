07585684a0a16cb29b7687bcda4fb66212297539
100472150 <100472150@alumnos.uc3m.es>
2023-05-05 16:55:02 +0200

    Probability tables funcion

--- Markov.py
+++ Markov.py
@@ -1,19 +1,38 @@
 
-f = open("T_Off.csv")
-lines = f.readlines()
-
-probabilities_on = []
-first_line = False
-
-for line in lines:
-    if not first_line:
-        first_line = True
-    else:
-        probabilities_on.append(line.split(",")[1:])
+def obtain_probabilites():
+    on_file = open("T_On.csv")
+    lines = on_file.readlines()
+    probabilities_on = []
 
 
-for i in range(len(probabilities_on)):
+    for line in lines:
+        probabilities_on.append(line.split(","))
+
+    for i in range(len(probabilities_on)):
+        for j in range(len(probabilities_on[i])):
+            probabilities_on[i][j] = float(probabilities_on[i][j])
         print(probabilities_on[i])
+
+
+    print("\n\n")
+
+    off_file = open("T_Off.csv")
+    lines = off_file.readlines()
+    probabilities_off = []
+
+    for line in lines:
+        probabilities_off.append(line.split(","))
+
+    for i in range(len(probabilities_off)):
+        for j in range(len(probabilities_off[i])):
+            probabilities_off[i][j] = float(probabilities_off[i][j])
+        print(probabilities_off[i])
+
+obtain_probabilites()
+
+
+
+
 
 
 
