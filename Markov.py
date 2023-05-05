caf47cb7e4971ba015e3c79fc72ecaf4281537ac
100472150 <100472150@alumnos.uc3m.es>
2023-05-05 16:16:41 +0200

    sd

--- Markov.py
+++ Markov.py
@@ -1,10 +1,19 @@
-import csv
-import csv
-with open('eggs.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
 
+f = open("T_On.csv")
+lines = f.readlines()
+
+probabilities_on = []
+first_line = False
+
+for line in lines:
+    if not first_line:
+        first_line = True
+    else:
+        probabilities_on.append(line.split(",")[1:])
+
+
+for i in range(len(probabilities_on)):
+        print(probabilities_on[i])
 
 
 
