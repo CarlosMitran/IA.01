09d9aaa7e5400b6670b5247ea85c8070e90fbade
Carlos Mitran <100472086@alumnos.uc3m.es>
2023-05-05 15:50:01 +0200

    csv files

---
+++ Markov.py
@@ -0,0 +1,12 @@
+import csv
+import csv
+with open('eggs.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+
+
+
+
+
+
