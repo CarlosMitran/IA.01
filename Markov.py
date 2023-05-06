71fe8199a050755dac35c32f25384c87c62000c0
Pablo <100472150@uc3m.alumnos.es>
2023-05-06 18:08:29 +0200

    Bellman finish

--- Markov.py
+++ Markov.py
@@ -30,42 +30,70 @@ def obtain_probabilites():
 def bellman(probabilies_on, probabilities_off, cost_on, cost_off):
     values = {}
     prev_values = {}
+    optimal_policy = {}
     init = 16.0
     for i in range(len(probabilies_on[0])):
         values["V("+str(init)+")"] = 0
         prev_values["V(" + str(init) + ")"] = 1
-
+        optimal_policy["V(" + str(init) + ")"] = None
         init += 0.5
 
-
+    #print(values)
+    #print("----------------------------------------------------")
     while prev_values != values:
-        prev_values = values
+        #hacer for para copiar valores y no igualarlo porque se convierte en un espejo
+
+        for key, value in values.items():
+            prev_values[key] = value
+
         counter_row = 0
         for key in values.keys():
-            print(values)
             counter_column = 0
             on = cost_on
             off = cost_off
             if key == "V(22.0)":
                 continue
-            for value in values.values():
+            for value in prev_values.values():
+                #if key == "V(16.0)" or key == "V(17.0)":
+                #print(counter_row, counter_column, probabilies_on[counter_row][counter_column], value)
                 on += probabilies_on[counter_row][counter_column] * value
                 off += probabilities_off[counter_row][counter_column] * value
+                on = round(on, 2)
+                off = round(off, 2)
                 #if key == "V(16.0)":
-                 #   print(probabilies_on[counter_row][counter_column], on)
+                #   print(probabilies_on[counter_row][counter_column], on)
 
                 counter_column += 1
 
             counter_row += 1
 
             values[key] = min(on, off)
-        print(prev_values)
-        print(values)
+            if on == min(on, off):
+                optimal_policy[key] = "on"
+            else:
+                optimal_policy[key] = "off"
+
+
+        #print(prev_values)
+        #print(values)
+        #print("----------------------------------------------------")
+
+    print("\n----------------------OPTIMAL POLICY------------------------------")
+    print(optimal_policy)
+
+
 
 
 
 prob_on, prob_off = obtain_probabilites()
-bellman(prob_on, prob_off, 3, 2)
+for i in range(1, 10):
+    for j in range(1, 10):
+        print("------------------------------------------------------------------")
+        print("Cost on: ", i,  "\nCost off: ", j)
+        bellman(prob_on, prob_off, i, j)
+
+
+
 
 
 
